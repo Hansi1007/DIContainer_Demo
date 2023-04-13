6319a9c8174a46c8c403c64b74b853ce361c16ec
Hans-Juergen Hahn <hans-juergen.hahn@mahle.com>
2023-04-13 10:02:26 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DIContainer_Demo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Date { get; set; } = string.Empty;        
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var _shortDate = new ShortDate();
+            var date = _shortDate.GetDate();
+            Date = date;
+            _logger.LogInformation("IndexModel OnGet Date: " + Date);
+        }
+    }
+}
